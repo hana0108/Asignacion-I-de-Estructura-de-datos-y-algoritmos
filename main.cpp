a7a31835a439ef1b1f0bfe7c99cd7ebf49662626
danielperez00190-commits <danielperez00190@gmail.com>
2026-05-15 17:24:39 -0600

    ejercicio 2
     Registro de Calificaciones en Bootcamp
    Una academia técnica gestiona las calificaciones de los
    estudiantes en 5 evaluaciones clave.
    Cada estudiante tiene: nombre, matrícula,
    calificaciones[], promedio y estado
    (aprobado/reprobado).
    Se debe:
    a) Registrar las notas por estudiante.
    b) Calcular promedio automático.
    c) Mostrar los estudiantes aprobados con
    promedio ≥ 70.
    d) Ordenar estudiantes por promedio.
    - Implementar usando un arreglo unidimensional
    de objetos Estudiante.

--- main.cpp
+++ main.cpp
@@ -3,33 +3,31 @@
 
 using namespace std;
 
-class Equipo
+class Estudiante
 {
 public:
     string nombre;
-    string institucion;
-    string categoria;
-    double puntaje;
-    string pais;
+    string matricula;
+    double calificaciones[5];
+    double promedio;
+    string estado;
 };
 
 int main()
 {
-    Equipo equipos[20];
+    Estudiante estudiantes[20];
     int contador = 0;
     int opcion;
 
     do
     {
         cout << "\n===== MENU =====" << endl;
-        cout << "1. Registrar equipos" << endl;
-        cout << "2. Mostrar equipos" << endl;
-        cout << "3. Ordenar por puntaje" << endl;
-        cout << "4. Mejor y peor equipo" << endl;
-        cout << "5. Filtrar por pais" << endl;
-        cout << "6. Contar nacionales por puntaje" << endl;
+        cout << "1. Registrar estudiante" << endl;
+        cout << "2. Mostrar estudiantes" << endl;
+        cout << "3. Mostrar aprobados" << endl;
+        cout << "4. Ordenar por promedio" << endl;
         cout << "0. Salir" << endl;
-        cout << "Seleccione: ";
+        cout << "Seleccione una opcion: ";
         cin >> opcion;
 
         switch(opcion)
@@ -41,25 +39,35 @@ int main()
                     cin.ignore();
 
                     cout << "Nombre: ";
-                    getline(cin, equipos[contador].nombre);
+                    getline(cin, estudiantes[contador].nombre);
 
-                    cout << "Institucion: ";
-                    getline(cin, equipos[contador].institucion);
+                    cout << "Matricula: ";
+                    getline(cin, estudiantes[contador].matricula);
 
-                    cout << "Categoria: ";
-                    getline(cin, equipos[contador].categoria);
+                    double suma = 0;
 
-                    cout << "Puntaje: ";
-                    cin >> equipos[contador].puntaje;
+                    for(int i = 0; i < 5; i++)
+                    {
+                        cout << "Calificacion " << i + 1 << ": ";
+                        cin >> estudiantes[contador].calificaciones[i];
 
-                    cin.ignore();
+                        suma += estudiantes[contador].calificaciones[i];
+                    }
 
-                    cout << "Pais: ";
-                    getline(cin, equipos[contador].pais);
+                    estudiantes[contador].promedio = suma / 5;
+
+                    if(estudiantes[contador].promedio >= 70)
+                    {
+                        estudiantes[contador].estado = "Aprobado";
+                    }
+                    else
+                    {
+                        estudiantes[contador].estado = "Reprobado";
+                    }
 
                     contador++;
 
-                    cout << "Equipo registrado.\n";
+                    cout << "Estudiante registrado.\n";
                 }
                 else
                 {
@@ -71,16 +79,21 @@ int main()
 
             case 2:
             {
-                cout << "\n=== EQUIPOS ===\n";
+                cout << "\n=== ESTUDIANTES ===\n";
 
                 for(int i = 0; i < contador; i++)
                 {
-                    cout << i + 1 << ". "
-                         << equipos[i].nombre << " | "
-                         << equipos[i].institucion << " | "
-                         << equipos[i].categoria << " | "
-                         << equipos[i].puntaje << " | "
-                         << equipos[i].pais << endl;
+                    cout << "\nNombre: "
+                         << estudiantes[i].nombre << endl;
+
+                    cout << "Matricula: "
+                         << estudiantes[i].matricula << endl;
+
+                    cout << "Promedio: "
+                         << estudiantes[i].promedio << endl;
+
+                    cout << "Estado: "
+                         << estudiantes[i].estado << endl;
                 }
 
                 break;
@@ -88,98 +101,39 @@ int main()
 
             case 3:
             {
-                for(int i = 0; i < contador - 1; i++)
+                cout << "\n=== ESTUDIANTES APROBADOS ===\n";
+
+                for(int i = 0; i < contador; i++)
                 {
-                    for(int j = i + 1; j < contador; j++)
+                    if(estudiantes[i].promedio >= 70)
                     {
-                        if(equipos[i].puntaje < equipos[j].puntaje)
-                        {
-                            Equipo aux = equipos[i];
-                            equipos[i] = equipos[j];
-                            equipos[j] = aux;
-                        }
+                        cout << estudiantes[i].nombre
+                             << " | Promedio: "
+                             << estudiantes[i].promedio
+                             << endl;
                     }
                 }
-
-                cout << "Equipos ordenados.\n";
 
                 break;
             }
 
             case 4:
             {
-                if(contador > 0)
+                for(int i = 0; i < contador - 1; i++)
                 {
-                    Equipo mejor = equipos[0];
-                    Equipo peor = equipos[0];
-
-                    for(int i = 1; i < contador; i++)
+                    for(int j = i + 1; j < contador; j++)
                     {
-                        if(equipos[i].puntaje > mejor.puntaje)
+                        if(estudiantes[i].promedio <
+                           estudiantes[j].promedio)
                         {
-                            mejor = equipos[i];
-                        }
-
-                        if(equipos[i].puntaje < peor.puntaje)
-                        {
-                            peor = equipos[i];
+                            Estudiante aux = estudiantes[i];
+                            estudiantes[i] = estudiantes[j];
+                            estudiantes[j] = aux;
                         }
                     }
-
-                    cout << "\n=== MEJOR EQUIPO ===\n";
-                    cout << mejor.nombre << " | "
-                         << mejor.puntaje << endl;
-
-                    cout << "\n=== PEOR EQUIPO ===\n";
-                    cout << peor.nombre << " | "
-                         << peor.puntaje << endl;
                 }
 
-                break;
-            }
-
-            case 5:
-            {
-                string buscar;
-
-                cin.ignore();
-
-                cout << "Ingrese pais: ";
-                getline(cin, buscar);
-
-                cout << "\n=== EQUIPOS DEL PAIS ===\n";
-
-                for(int i = 0; i < contador; i++)
-                {
-                    if(equipos[i].pais == buscar)
-                    {
-                        cout << equipos[i].nombre << " | "
-                             << equipos[i].puntaje << endl;
-                    }
-                }
-
-                break;
-            }
-
-            case 6:
-            {
-                double minimo;
-                int cantidad = 0;
-
-                cout << "Puntaje minimo: ";
-                cin >> minimo;
-
-                for(int i = 0; i < contador; i++)
-                {
-                    if(equipos[i].pais == "Republica Dominicana"
-                       && equipos[i].puntaje > minimo)
-                    {
-                        cantidad++;
-                    }
-                }
-
-                cout << "Equipos nacionales mayores al puntaje: "
-                     << cantidad << endl;
+                cout << "Estudiantes ordenados.\n";
 
                 break;
             }
